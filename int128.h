ae09a0632b5aee3a78c580e0e24db13b7ffa8f11
Ayman El Didi <ayman@eldidi.org>
2023-03-21 12:37:19 -0600

    finished implementation, but still testing

--- int128.h
+++ int128.h
@@ -1,14 +1,18 @@
 #ifndef INT128_H
 #define INT128_H
+#include <inttypes.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-#define INT128_MAX ((int128){ UINT64_C(0x7fffffffffffffff), UINT64_MAX })
-#define INT128_MIN ((int128){ UINT64_C(0x8000000000000000), 0 })
-#define UINT128_MAX ((uint128){ UINT64_MAX, UINT64_MAX })
+#define INT128_MAX \
+	((int128){ .low = UINT64_MAX, .high = UINT64_C(0x7fffffffffffffff) })
+#define INT128_MIN ((int128){ .low = 0, .high = UINT64_C(0x8000000000000000) })
+#define UINT128_MAX ((uint128){ .low = UINT64_MAX, .high = UINT64_MAX })
 
 #define UINT128_C(x) ((uint128){ .low = x })
-#define INT128_C(x) \
-	((x < 0) ? (int128){ .low = x, .high = UINT64_C(~0) } : (int128){ x })
+#define INT128_C(x)                                             \
+	((x < 0) ? (int128){ .low = x, .high = UINT64_C(~0) } : \
+		   (int128){ .low = x })
 
 struct int128 {
 	uint64_t low;
@@ -22,6 +26,110 @@ struct uint128 {
 
 typedef struct int128 int128;
 typedef struct uint128 uint128;
+
+// Returns lhs < rhs.
+static inline bool uint128_less(uint128 lhs, uint128 rhs)
+{
+	if (lhs.high == rhs.high) {
+		return lhs.low < rhs.low;
+	}
+
+	return lhs.high < rhs.high;
+}
+
+// Returns lhs > rhs.
+static inline bool uint128_greater(uint128 lhs, uint128 rhs)
+{
+	if (lhs.high == rhs.high) {
+		return lhs.low > rhs.low;
+	}
+
+	return lhs.high > rhs.high;
+}
+
+// Returns lhs < rhs.
+static inline bool int128_less(int128 lhs, int128 rhs)
+{
+	union {
+		uint64_t u;
+		int64_t i;
+	} cvt;
+	union {
+		uint64_t u;
+		int64_t i;
+	} cvt2;
+
+	cvt.u = lhs.high;
+	cvt2.u = rhs.high;
+
+	if (cvt.i == cvt2.i) {
+		cvt.u = lhs.low;
+		cvt2.u = rhs.low;
+		return cvt.i < cvt2.i;
+	}
+
+	return cvt.i < cvt2.i;
+}
+
+// Returns lhs > rhs.
+static inline bool int128_greater(int128 lhs, int128 rhs)
+{
+	union {
+		uint64_t u;
+		int64_t i;
+	} cvt;
+	union {
+		uint64_t u;
+		int64_t i;
+	} cvt2;
+
+	cvt.u = lhs.high;
+	cvt2.u = rhs.high;
+
+	if (cvt.i == cvt2.i) {
+		cvt.u = lhs.low;
+		cvt2.u = rhs.low;
+		return cvt.i > cvt2.i;
+	}
+
+	return cvt.i > cvt2.i;
+}
+
+// Returns lhs == rhs.
+static inline bool int128_eq(int128 lhs, int128 rhs)
+{
+	return lhs.low == rhs.low && lhs.high == rhs.high;
+}
+
+// Returns lhs == rhs.
+static inline bool uint128_eq(uint128 lhs, uint128 rhs)
+{
+	return lhs.low == rhs.low && lhs.high == rhs.high;
+}
+
+// Returns lhs <= rhs.
+static inline bool uint128_less_eq(uint128 lhs, uint128 rhs)
+{
+	return uint128_less(lhs, rhs) || uint128_eq(lhs, rhs);
+}
+
+// Returns lhs >= rhs.
+static inline bool uint128_greater_eq(uint128 lhs, uint128 rhs)
+{
+	return uint128_greater(lhs, rhs) || uint128_eq(lhs, rhs);
+}
+
+// Returns lhs >= rhs.
+static inline bool int128_greater_eq(int128 lhs, int128 rhs)
+{
+	return int128_greater(lhs, rhs) || int128_eq(lhs, rhs);
+}
+
+// Returns lhs <= rhs.
+static inline bool int128_less_eq(int128 lhs, int128 rhs)
+{
+	return int128_less(lhs, rhs) || int128_eq(lhs, rhs);
+}
 
 // Adds two int128s, wrapping on overflow.
 static inline int128 int128_add(int128 lhs, int128 rhs)
@@ -74,6 +182,118 @@ static inline uint128 uint128_compl(uint128 x)
 	return (uint128){
 		.low = ~x.low,
 		.high = ~x.high,
+	};
+}
+
+// Returns lhs | rhs.
+static inline int128 int128_or(int128 lhs, int128 rhs)
+{
+	return (int128){
+		.low = lhs.low | rhs.low,
+		.high = lhs.high | rhs.high,
+	};
+}
+
+// Returns lhs & rhs.
+static inline int128 int128_and(int128 lhs, int128 rhs)
+{
+	return (int128){
+		.low = lhs.low & rhs.low,
+		.high = lhs.high & rhs.high,
+	};
+}
+
+// Returns lhs | rhs.
+static inline uint128 uint128_or(uint128 lhs, uint128 rhs)
+{
+	return (uint128){
+		.low = lhs.low | rhs.low,
+		.high = lhs.high | rhs.high,
+	};
+}
+
+// Returns lhs & rhs.
+static inline uint128 uint128_and(uint128 lhs, uint128 rhs)
+{
+	return (uint128){
+		.low = lhs.low & rhs.low,
+		.high = lhs.high & rhs.high,
+	};
+}
+
+// Returns lhs ^ rhs.
+static inline int128 int128_xor(int128 lhs, int128 rhs)
+{
+	return (int128){
+		.low = lhs.low ^ rhs.low,
+		.high = lhs.high ^ rhs.high,
+	};
+}
+
+// Returns lhs ^ rhs.
+static inline uint128 uint128_xor(uint128 lhs, uint128 rhs)
+{
+	return (uint128){
+		.low = lhs.low ^ rhs.low,
+		.high = lhs.high ^ rhs.high,
+	};
+}
+
+// Returns lhs << rhs.
+static inline int128 int128_shiftl(int128 lhs, int rhs)
+{
+	if (rhs >= 64) {
+		return (int128){ .low = 0, .high = lhs.low << (rhs - 64) };
+	}
+
+	return (int128){
+		.low = lhs.low << rhs,
+		.high = (lhs.high << rhs) | (lhs.low >> (64 - rhs)),
+	};
+}
+
+// Returns lhs << rhs.
+static inline uint128 uint128_shiftl(uint128 lhs, int rhs)
+{
+	int128 a = { .low = lhs.low, .high = lhs.high };
+	int128 result = int128_shiftl(a, rhs);
+	return (uint128){ .low = result.low, .high = result.high };
+}
+
+// Returns lhs >> rhs.
+static inline uint128 uint128_shiftr(uint128 lhs, int rhs)
+{
+	if (rhs >= 64) {
+		return (uint128){ .low = lhs.high >> (rhs - 64), .high = 0 };
+	}
+
+	return (uint128){
+		.low = (lhs.low >> rhs) | (lhs.high << (64 - rhs)),
+		.high = (lhs.high >> rhs),
+	};
+}
+
+// Returns lhs >> rhs.
+// On signed int128 this is an arithmetic shift, so the sign bit is used for
+// shifted in bits.
+static inline int128 int128_shiftr(int128 lhs, int rhs)
+{
+	if (int128_greater(lhs, INT128_C(0))) {
+		uint128 a = { .low = lhs.low, .high = lhs.high };
+		uint128 result = uint128_shiftr(a, rhs);
+		return (int128){ .low = result.low, .high = result.high };
+	}
+
+	if (rhs >= 64) {
+		return (int128){
+			.low = (lhs.high >> (rhs - 64)) | (UINT64_C(~0) << rhs),
+			.high = UINT64_C(~0),
+		};
+	}
+
+	return (int128){
+		.low = (lhs.low >> rhs) | (lhs.high << (64 - rhs)),
+		.high = (lhs.high >> rhs) | (UINT64_C(~0) << (64 - rhs)),
 	};
 }
 
@@ -159,38 +379,7 @@ static inline int128 int128_mul64(int64_t lhs, int64_t rhs)
 	return result;
 }
 
-static inline bool uint128_less(uint128 lhs, uint128 rhs)
-{
-	if (lhs.high == rhs.high) {
-		return lhs.low < rhs.low;
-	}
-
-	return lhs.high < rhs.high;
-}
-
-static inline bool int128_less(int128 lhs, int128 rhs)
-{
-	union {
-		uint64_t u;
-		int64_t i;
-	} cvt;
-	union {
-		uint64_t u;
-		int64_t i;
-	} cvt2;
-
-	cvt.u = lhs.high;
-	cvt2.u = rhs.high;
-
-	if (cvt.i == cvt2.i) {
-		cvt.u = lhs.low;
-		cvt2.u = rhs.low;
-		return cvt.i < cvt2.i;
-	}
-
-	return cvt.i < cvt2.i;
-}
-
+// Returns lhs * rhs.
 static inline uint128 uint128_mul(uint128 lhs, uint128 rhs)
 {
 	uint128 result = uint128_mul64(lhs.low, rhs.low);
@@ -198,6 +387,7 @@ static inline uint128 uint128_mul(uint128 lhs, uint128 rhs)
 	return result;
 }
 
+// Returns lhs * rhs.
 static inline int128 int128_mul(int128 lhs, int128 rhs)
 {
 	bool result_negative = false;
@@ -223,6 +413,140 @@ static inline int128 int128_mul(int128 lhs, int128 rhs)
 	return result;
 }
 
-// TODO: division, modulo, bit operations, greater than, equal.
+// Returns lhs / rhs.
+static inline uint128 uint128_div(uint128 lhs, uint128 rhs)
+{
+	// Based on the algorithm described here:
+	// https://stackoverflow.com/questions/5386377/division-without-using
+	if (uint128_greater(rhs, lhs)) {
+		return UINT128_C(0);
+	}
+
+	if (uint128_eq(lhs, rhs)) {
+		return UINT128_C(1);
+	}
+
+	uint128 current = UINT128_C(1);
+	uint128 result = UINT128_C(0);
+	uint128 divisor = rhs;
+	while (uint128_less_eq(divisor, lhs)) {
+		divisor = uint128_shiftl(divisor, 1);
+		current = uint128_shiftl(current, 1);
+	}
+
+	divisor = uint128_shiftr(divisor, 1);
+	current = uint128_shiftr(current, 1);
+
+	while (!uint128_eq(current, UINT128_C(0))) {
+		if (uint128_greater_eq(lhs, divisor)) {
+			lhs = uint128_sub(lhs, divisor);
+			result = uint128_or(result, current);
+		}
+
+		current = uint128_shiftr(current, 1);
+		divisor = uint128_shiftr(divisor, 1);
+	}
+
+	return result;
+}
+
+// Returns lhs % rhs.
+static inline uint128 uint128_mod(uint128 lhs, uint128 rhs)
+{
+	// Based on the algorithm described here:
+	// https://stackoverflow.com/questions/5386377/division-without-using
+	if (uint128_greater(rhs, lhs)) {
+		return lhs;
+	}
+
+	if (uint128_eq(lhs, rhs)) {
+		return UINT128_C(0);
+	}
+
+	uint128 current = UINT128_C(1);
+	uint128 result = UINT128_C(0);
+	uint128 divisor = rhs;
+	while (uint128_less_eq(divisor, lhs)) {
+		divisor = uint128_shiftl(divisor, 1);
+		current = uint128_shiftl(current, 1);
+	}
+
+	divisor = uint128_shiftr(divisor, 1);
+	current = uint128_shiftr(current, 1);
+
+	while (!uint128_eq(current, UINT128_C(0))) {
+		if (uint128_greater_eq(lhs, divisor)) {
+			lhs = uint128_sub(lhs, divisor);
+			result = uint128_or(result, current);
+		}
+
+		current = uint128_shiftr(current, 1);
+		divisor = uint128_shiftr(divisor, 1);
+	}
+
+	return lhs;
+}
+
+// Returns lhs / rhs.
+static inline int128 int128_div(int128 lhs, int128 rhs)
+{
+	// signed integers aren't symmetric. INT128_MIN == (-INT128_MAX) - 1
+	if (int128_eq(lhs, INT128_MIN)) {
+		return INT128_C(0);
+	}
+
+	bool result_negative = false;
+	if (int128_less(lhs, INT128_C(0))) {
+		result_negative = !result_negative;
+		lhs = int128_neg(lhs);
+	}
+
+	if (int128_less(rhs, INT128_C(0))) {
+		result_negative = !result_negative;
+		rhs = int128_neg(rhs);
+	}
+
+	uint128 a = { .low = lhs.low, .high = lhs.high };
+	uint128 b = { .low = rhs.low, .high = rhs.low };
+	uint128 c = uint128_div(a, b);
+	int128 result = { .low = c.low, .high = c.high };
+
+	if (result_negative) {
+		return int128_neg(result);
+	}
+
+	return result;
+}
+
+// Returns lhs % rhs.
+static inline int128 int128_mod(int128 lhs, int128 rhs)
+{
+	// signed integers aren't symmetric. INT128_MIN == (-INT128_MAX) - 1
+	if (int128_eq(lhs, INT128_MIN)) {
+		return INT128_C(0);
+	}
+
+	bool result_negative = false;
+	if (int128_less(lhs, INT128_C(0))) {
+		result_negative = !result_negative;
+		lhs = int128_neg(lhs);
+	}
+
+	if (int128_less(rhs, INT128_C(0))) {
+		result_negative = !result_negative;
+		rhs = int128_neg(rhs);
+	}
+
+	uint128 a = { .low = lhs.low, .high = lhs.high };
+	uint128 b = { .low = rhs.low, .high = rhs.low };
+	uint128 c = uint128_mod(a, b);
+	int128 result = { .low = c.low, .high = c.high };
+
+	if (result_negative) {
+		return int128_neg(result);
+	}
+
+	return result;
+}
 
 #endif // INT128_H
---
+++ test.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <assert.h>
+
+#include "int128.h"
+
+void int128_tests()
+{
+	// Addition Sanity Check
+	int128 a = INT128_C(UINT64_MAX);
+	int128 b = int128_add(a, INT128_C(1));
+	assert(int128_greater(b, a));
+	assert(int128_less(b, int128_add(a, INT128_C(2))));
+	assert(!int128_eq(b, a));
+
+	// Subtraction Sanity Check
+	b = int128_sub(b, INT128_C(1));
+	assert(int128_eq(b, INT128_C(UINT64_MAX)));
+
+	// Does addition wrap on overflow properly?
+	b = int128_add(INT128_MAX, INT128_C(1));
+	assert(int128_less(b, INT128_MAX));
+	assert(int128_eq(b, INT128_MIN));
+
+	// Does subtraction wrap on underflow properly?
+	b = int128_sub(INT128_MIN, INT128_C(1));
+	assert(int128_eq(b, INT128_MAX));
+	assert(int128_eq(b, INT128_MAX));
+
+	// Multiplication Sanity Check
+	a = (int128){ .high = 100000000 };
+	b = int128_mul(a, INT128_C(2));
+	assert(b.high == 200000000);
+	assert(b.low == 0);
+
+	b = int128_mul(INT128_MAX, INT128_C(0));
+	assert(int128_eq(b, INT128_C(0)));
+
+	// Division Sanity Check
+	a = (int128){ .high = 200000000 };
+	b = int128_div(a, INT128_C(2));
+	printf("%" PRId64 "%.20" PRIu64 " / 2 = %" PRId64 "%.20" PRIu64 "\n",
+	       a.high, a.low, b.high, b.low);
+	assert(b.high == 100000000);
+}
+
+int main()
+{
+	int128_tests();
+}
